026651c6e1065b2266760257dbce759e18d9bb54
Lucio Valentin <lgvalentin@utfpr.edu.br>
2016-08-09 21:24:35 -0300

    Enable favicon in subfolder  /dacom/favicon.ico

--- php/index.php
+++ php/index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title>Presença DACOM - v0.1b</title>
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	<!--script src="//code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
